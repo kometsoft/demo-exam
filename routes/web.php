70df32280f626426e7a6ca52ae31f721164a56c2
muhaizi <muhaizi@kometsoft.com.my>
2023-09-28 13:13:51 +0800

    u

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 });
 
 Route::resource('/complaint', \App\Http\Controllers\ComplaintController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
